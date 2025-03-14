0b28452c60bbe295534d4d3485d017ec0128959f
Tamires <tamiresmanhaes4@gmail.com>
2025-03-14 11:41:26 -0300

    add tailwind-scrollbar plugin and update dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: ['./src/**/*.tsx'],
   theme: {
     extend: {}
   },
-  plugins: []
+  plugins: [
+    require('tailwind-scrollbar')
+  ]
 }
