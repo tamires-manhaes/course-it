cc65c4a8321b9c6c75c94e7daa0b286356c53248
Tamires <tamiresmanhaes4@gmail.com>
2025-03-15 14:39:42 -0300

    feat: Enhance Lesson component with current state indication and update Module to track active lesson

--- src/components/Module.tsx
+++ src/components/Module.tsx
@@ -15,6 +15,11 @@ interface ModuleProps {
 export function Module({ title, lessonsAmount, moduleIndex }: ModuleProps){
   const dispatch = useDispatch()
 
+  const { currentLessonIndex, currentModuleIndex } = useAppSelector(state => {
+    const { currentLessonIndex, currentModuleIndex } = state.player
+    return { currentLessonIndex, currentModuleIndex }
+  })
+
   const lessons = useAppSelector(state => {
     return state.player.course.modules[moduleIndex].lessons
   })
@@ -32,12 +37,14 @@ export function Module({ title, lessonsAmount, moduleIndex }: ModuleProps){
       <Collapsible.Content >
         <nav className="relative flex flex-col gap-4 p-6">
           {lessons.map((lesson, lessonIndex) => {
+            const isCurrent = currentModuleIndex === moduleIndex && currentLessonIndex === lessonIndex
             return (
               <Lesson 
                 key={lesson.id} 
                 title={lesson.title} 
                 duration={lesson.duration} 
                 onPlay={() => dispatch(play([moduleIndex, lessonIndex]))}
+                isCurrent={isCurrent}
               />
             )
           })}
