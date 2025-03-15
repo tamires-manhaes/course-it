c81cb7bbc9a0b924322255c5a2355eb27df7d6df
Tamires <tamiresmanhaes4@gmail.com>
2025-03-15 14:28:44 -0300

    feat: Update VideoPlayer component to dynamically load video URL based on current lesson

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -1,13 +1,21 @@
 import ReactPlayer from 'react-player'
+import { useAppSelector } from '../store'
 
 export function VideoPlayer(){
+  const lesson = useAppSelector(state => {
+    const { currentLessonIndex, currentModuleIndex } = state.player
+
+    const currentLesson = state.player.course.modules[currentModuleIndex].lessons[currentLessonIndex]
+
+    return currentLesson
+  })
   return (
     <div className="w-full bg-zinc-950 aspect-video">
       <ReactPlayer 
         width="100%"
         height="100%"
         controls
-        url="https://www.youtube.com/watch?v=QCPJ0VdpM00"
+        url={`https://www.youtube.com/watch?v=${lesson.id}`}
       />
     </div>
   )
