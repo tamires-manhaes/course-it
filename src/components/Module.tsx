87fcf561b9338fde924d1b05bd8a45dc5849e8d8
Tamires <tamiresmanhaes4@gmail.com>
2025-03-15 14:14:16 -0300

    refactor: Module and Player components to dynamically render lessons and modules from Redux store

--- src/components/Module.tsx
+++ src/components/Module.tsx
@@ -2,6 +2,7 @@ import * as Collapsible from '@radix-ui/react-collapsible'
 
 import { ChevronDown } from "lucide-react";
 import { Lesson } from "./Lesson";
+import { useAppSelector } from '../store';
 
 interface ModuleProps {
   title: string
@@ -10,10 +11,13 @@ interface ModuleProps {
 }
 
 export function Module({ title, lessonsAmount, moduleIndex }: ModuleProps){
+  const lessons = useAppSelector(state => {
+    return state.player.course.modules[moduleIndex].lessons
+  })
   return (
     <Collapsible.Root className='group'>
       <Collapsible.Trigger className="flex w-full items-center gap-3 bg-zinc-800 p-4 cursor-pointer">
-        <div className="flex h-10 w-10 rounded-full items-center justify-center bg-zinc-950 text-xs">{moduleIndex + 1}</div>
+        <div className="flex h-10 w-10 rounded-full items-center justify-center bg-zinc-950 text-sm">{moduleIndex + 1}</div>
         <div className="flex flex-col gap-1 text-left">
           <strong className="text-sm">{title}</strong>
           <span className="text-xs text-zinc-400">{lessonsAmount} aulas</span>
@@ -23,9 +27,15 @@ export function Module({ title, lessonsAmount, moduleIndex }: ModuleProps){
 
       <Collapsible.Content >
         <nav className="relative flex flex-col gap-4 p-6">
-          <Lesson title="Fundamentos do Redux" duration="09:13"/>
-          <Lesson title="Reducers" duration="05:30"/>
-          <Lesson title="Stores" duration="03:13"/>
+          {lessons.map((lesson) => {
+            return (
+              <Lesson 
+                key={lesson.id} 
+                title={lesson.title} 
+                duration={lesson.duration} 
+              />
+            )
+          })}
         </nav>
       </Collapsible.Content>
     </Collapsible.Root>
--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -3,27 +3,39 @@ import { MessageCircle } from "lucide-react";
 import { Header } from "../components/Header";
 import { VideoPlayer } from "../components/VideoPlayer";
 import { Module } from "../components/Module";
+import { useAppSelector } from "../store";
 
 export function Player(){
+  const modules = useAppSelector(state => state.player.course.modules)
+
   return (
     <div className="h-screen bg-zinc-950 text-zinc-50 flex justify-center items-center">
       <div className="flex w-[1100px] flex-col gap-6">
         <div className="flex items-center justify-between">
           <Header />
+
           <button className="flex items-center gap-2 rounded bg-violet-500 px-3 py-2 text-sm font-medium text-white hover:bg-violet-600">
             <MessageCircle className="w-4 h-4"/> 
             Deixar feedback
           </button>
         </div>
+
         <main className="relative flex overflow-hidden rounded-lg border border-zinc-800 bg-zinc-900 shadow pr-80">
           <div className="flex-1">
             <VideoPlayer />
           </div>
+          
           <aside className="w-80 absolute top-0 bottom-0 right-0 border-l border-zinc-800 bg-zinc-900 divide-y-1 divide-zinc-900 overflow-y-scroll scrollbar scrollbar-thin scrollbar-track-zinc-950 scrollbar-thumb-zinc-800">
-            <Module moduleIndex={0} title="Fundamentos" lessonsAmount={3} />
-            <Module moduleIndex={1} title="Arquitetura Flux" lessonsAmount={7} />
-            <Module moduleIndex={2} title="Estados e Actions" lessonsAmount={5} />
-            <Module moduleIndex={3} title="Fetch and Erro Handler" lessonsAmount={7} />
+            {modules.map((module, index) => {
+              return (
+                <Module 
+                  key={module.id} 
+                  moduleIndex={index} 
+                  title={module.title} 
+                  lessonsAmount={module.lessons.length} 
+                />
+              )
+            })}
           </aside>
         </main>
       </div>
