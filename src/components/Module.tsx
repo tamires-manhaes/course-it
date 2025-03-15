4a5ad8a6dd283991584a2ad5a2cf81d7d14f4a85
Tamires <tamiresmanhaes4@gmail.com>
2025-03-15 14:28:50 -0300

    feat: Integrate play action in Module component to dispatch lesson play event

--- src/components/Module.tsx
+++ src/components/Module.tsx
@@ -3,6 +3,8 @@ import * as Collapsible from '@radix-ui/react-collapsible'
 import { ChevronDown } from "lucide-react";
 import { Lesson } from "./Lesson";
 import { useAppSelector } from '../store';
+import { useDispatch } from 'react-redux';
+import { play } from '../store/slices/player';
 
 interface ModuleProps {
   title: string
@@ -11,6 +13,8 @@ interface ModuleProps {
 }
 
 export function Module({ title, lessonsAmount, moduleIndex }: ModuleProps){
+  const dispatch = useDispatch()
+
   const lessons = useAppSelector(state => {
     return state.player.course.modules[moduleIndex].lessons
   })
@@ -27,12 +31,13 @@ export function Module({ title, lessonsAmount, moduleIndex }: ModuleProps){
 
       <Collapsible.Content >
         <nav className="relative flex flex-col gap-4 p-6">
-          {lessons.map((lesson) => {
+          {lessons.map((lesson, lessonIndex) => {
             return (
               <Lesson 
                 key={lesson.id} 
                 title={lesson.title} 
                 duration={lesson.duration} 
+                onPlay={() => dispatch(play([moduleIndex, lessonIndex]))}
               />
             )
           })}
