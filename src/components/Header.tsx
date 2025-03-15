9da7c394a2c0f52e36b5f85bf799301e0317fc68
Tamires <tamiresmanhaes4@gmail.com>
2025-03-15 14:39:46 -0300

    feat: Update Header component to display current lesson and module titles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,16 @@
+import { useAppSelector } from "../store"
+
 export function Header() {
+   const {currentLesson, currentModule} = useAppSelector(state => {
+      const { currentLessonIndex, currentModuleIndex } = state.player
+      const currentLesson = state.player.course.modules[currentModuleIndex].lessons[currentLessonIndex]
+      const currentModule = state.player.course.modules[currentModuleIndex]
+      return {currentModule, currentLesson }
+    })
   return (
     <div className="flex flex-col gap-1 ">
-      <h1 className="text-2xl font-bold">Fundamentos do Redux</h1>
-      <span className="text-sm text-zinc-400">Modulo - Desvendando o Redux</span>
+      <h1 className="text-2xl font-bold">{currentLesson.title}</h1>
+      <span className="text-sm text-zinc-400">Modulo - {currentModule.title}</span>
     </div>
   )
 }
